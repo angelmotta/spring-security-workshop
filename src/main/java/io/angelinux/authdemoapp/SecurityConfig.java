84f7e9cdcfab567aebae70204737a52e78efe64b
Angel Motta <angel.motta@utec.edu.pe>
2024-01-24 22:45:47 -0500

    feat: http readirect form login

--- src/main/java/io/angelinux/authdemoapp/SecurityConfig.java
+++ src/main/java/io/angelinux/authdemoapp/SecurityConfig.java
@@ -2,8 +2,12 @@ package io.angelinux.authdemoapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +16,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
+        http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/private").authenticated()
                 .anyRequest().permitAll()
         );
+        http.formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password("{noop}password")
+                        .authorities("ROLE_user")
+                        .build()
+        );
     }
 }
