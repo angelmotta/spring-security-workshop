c0a71c9635e47e6f3acb036e77be8e95e3ef010e
Angel Motta <angel.motta@utec.edu.pe>
2024-01-26 16:39:42 -0500

    feat: get user information

--- src/main/java/io/angelinux/authdemoapp/WebController.java
+++ src/main/java/io/angelinux/authdemoapp/WebController.java
@@ -1,7 +1,12 @@
 package io.angelinux.authdemoapp;
 
+import org.ietf.jgss.Oid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class WebController {
@@ -12,7 +17,16 @@ public class WebController {
     }
 
     @GetMapping("/private")
-    public String privatePage() {
-        return "This should be private 🤔";
+    public String privatePage(Authentication authentication) {
+        //return "Welcome to the VIP Room [ " + authentication.getName() + " ] 😃☕️🎉";
+        return "Welcome to the VIP Room [ " + getName(authentication) + " ] 😃☕️🎉";
+    }
+
+    private static String getName(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail)
+                .orElseGet(authentication::getName);
     }
 }
