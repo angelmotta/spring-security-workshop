9d6a61726c8841f315800a814ae4d87e41a25339
Angel Motta <angel.motta@utec.edu.pe>
2024-01-27 19:52:44 -0500

    feat: adding custom filter in the filterChain

--- src/main/java/io/angelinux/authdemoapp/SecurityConfig.java
+++ src/main/java/io/angelinux/authdemoapp/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +25,7 @@ public class SecurityConfig {
         );
         http.formLogin(Customizer.withDefaults());
         http.oauth2Login(Customizer.withDefaults());
+        http.addFilterBefore(new RobotFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
